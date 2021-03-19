0372299286325e8b13bf06f4e9bc39d69b0ae163
Tushargupta9800 <tushargupta9800@gmail.com>
2021-03-19 21:35:50 +0530

    remove unwanted await

--- lib/widgets/toast.dart
+++ lib/widgets/toast.dart
@@ -2,5 +2,5 @@ import 'package:flutter/cupertino.dart';
 import 'package:toast/toast.dart';
 
 Future<void> ShowToast(String text, BuildContext context) async {
-  await Toast.show(text, context, duration: Toast.LENGTH_SHORT, gravity:  Toast.BOTTOM);
+  Toast.show(text, context, duration: Toast.LENGTH_SHORT, gravity:  Toast.BOTTOM);
 }
